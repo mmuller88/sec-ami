ae96d96e1a92037904798d3bf783acae9b54409f
Martin Mueller <damadden88@googlemail.com>
2021-08-10 20:13:34 +0200

    fix: ami release script improvements

--- createAmi.sh
+++ createAmi.sh
@@ -3,7 +3,9 @@
 AWS_ACCESS_KEY_ID=$(aws secretsmanager get-secret-value --secret-id build/credentials --query SecretString | jq -r 'fromjson | ."aws_access_key_id"')
 AWS_SECRET_ACCESS_KEY=$(aws secretsmanager get-secret-value --secret-id build/credentials --query SecretString | jq -r 'fromjson | ."aws_secret_access_key"')
 
-sed -e 's;##aws_access_key_id##;'$AWS_ACCESS_KEY_ID';g' -e 's;##aws_secret_access_key##;'$AWS_SECRET_ACCESS_KEY';g' .docker-build-ami_REPLACE.conf > .docker-build-ami.conf
+AMI_VERSION=v1.0.2
+
+sed -e 's;##aws_access_key_id##;'$AWS_ACCESS_KEY_ID';g' -e 's;##aws_secret_access_key##;'$AWS_SECRET_ACCESS_KEY';g' -e 's;##ami_version##;'$AMI_VERSION';g' .docker-build-ami_REPLACE.conf > .docker-build-ami.conf
 
 docker-build-ami -c .docker-build-ami.conf | tee docker-build-ami.log
 sleep 3
@@ -12,3 +14,5 @@ cat docker-build-ami.log
 AMI=$(cat docker-build-ami.log| grep "Created image: " | awk '{print $3}')
 echo "AMI: $AMI"
 aws ec2 modify-image-attribute --image-id $AMI --launch-permission "Add=[{UserId=890129220607}, {UserId=681044435084}]" --region us-east-1
+
+# ami marketplace for publishing
