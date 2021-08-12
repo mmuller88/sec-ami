5b78e1cbe9559a1f6e4d69178f15043b4138a708
Martin Mueller <damadden88@googlemail.com>
2021-08-12 09:00:20 +0200

    Update createAmi script with getting the tag version automatically

--- createAmi.sh
+++ createAmi.sh
@@ -3,7 +3,8 @@
 AWS_ACCESS_KEY_ID=$(aws secretsmanager get-secret-value --secret-id build/credentials --region eu-central-1 --query SecretString | jq -r 'fromjson | ."aws_access_key_id"')
 AWS_SECRET_ACCESS_KEY=$(aws secretsmanager get-secret-value --secret-id build/credentials --region eu-central-1 --query SecretString | jq -r 'fromjson | ."aws_secret_access_key"')
 
-AMI_VERSION=v1.0.9
+git pull
+AMI_VERSION=$(git describe --abbrev=0 --tags)
 
 sed -e 's;##aws_access_key_id##;'$AWS_ACCESS_KEY_ID';g' -e 's;##aws_secret_access_key##;'$AWS_SECRET_ACCESS_KEY';g' -e 's;##ami_version##;'$AMI_VERSION';g' .docker-build-ami_REPLACE.conf > .docker-build-ami.conf
 
